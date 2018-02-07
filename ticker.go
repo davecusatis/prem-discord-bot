aae0453f78851ecb0d9f368cb26092a6afac925c
Dave Cusatis <davecusatis1@gmail.com>
2018-02-06 23:30:46 -0800

    Add top ten command

--- ticker.go
+++ ticker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"strings"
@@ -14,7 +15,7 @@ const (
 	PriceCheckCMD = ".price"
 
 	// TopTenCMD is the command to check the top 10 on cmc
-	TopTenCMD = ".topten"
+	TopTenCMD = ".top"
 
 	// SpaceDelimiter is the delimiter to split commands on
 	SpaceDelimiter = " "
@@ -24,8 +25,7 @@ func messageHandler(disc *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == disc.State.User.ID {
 		return
 	}
-
-	if !strings.Contains(m.Content, PriceCheckCMD) {
+	if !strings.Contains(m.Content, PriceCheckCMD) && !strings.Contains(m.Content, TopTenCMD) {
 		return
 	}
 
@@ -38,10 +38,13 @@ func messageHandler(disc *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	// if cmdArray[0] == TopTenCMD {
-	// 	err := sendTopTenInfoMessage(disc)
-	// 	return
-	// }
+	if cmdArray[0] == TopTenCMD {
+		err := sendTopTenInfoMessage(disc)
+		if err != nil {
+			log.Printf("Error checking top ten %s", err)
+		}
+		return
+	}
 }
 
 func sendPriceCheckMessage(disc *discordgo.Session, channelID, ticker string) error {
@@ -95,53 +98,31 @@ func sendPriceCheckMessage(disc *discordgo.Session, channelID, ticker string) er
 	return nil
 }
 
-// func sendTopTenInfoMessage(disc *discordgo.Session) error {
-// 	coinInfo, err := coinApi.GetAllCoinData(10)
-// 	if err != nil {
-// 		_, _ = disc.ChannelMessageSend(channelID, fmt.Sprintf("Error getting info for %s from coinmarketcap. Please try again later.", ticker))
-// 		return err
-// 	}
+func generateTopTenMessage(coins map[string]coinApi.Coin) string {
+	var rankMap map[int]string
+	for _, coin := range coins {
+		rankMap[coin.Rank] = fmt.Sprintf("%d. %s $%.2f\n", coin.Rank, coin.Name, coin.PriceUsd)
+	}
 
-// 	fields := make([]*discordgo.MessageEmbedField, 10)
+	i := 1
+	var buffer bytes.Buffer
+	for i <= 10 {
+		buffer.WriteString(rankMap[i])
+	}
 
-// 	fields[0] = new(discordgo.MessageEmbedField)
-// 	fields[0].Name = "Price USD"
-// 	fields[0].Value = fmt.Sprint(coinInfo.PriceUsd)
-// 	fields[0].Inline = true
+	return buffer.String()
+}
 
-// 	fields[1] = new(discordgo.MessageEmbedField)
-// 	fields[1].Name = "Price BTC"
-// 	fields[1].Value = fmt.Sprint(coinInfo.PriceBtc)
-// 	fields[1].Inline = true
-
-// 	fields[2] = new(discordgo.MessageEmbedField)
-// 	fields[2].Name = "Market Cap"
-// 	fields[2].Value = fmt.Sprint(coinInfo.MarketCapUsd)
-// 	fields[2].Inline = true
-
-// 	fields[3] = new(discordgo.MessageEmbedField)
-// 	fields[3].Name = "Percentage Change 1hr"
-// 	fields[3].Value = fmt.Sprint(coinInfo.PercentChange1h)
-// 	fields[3].Inline = true
-
-// 	fields[4] = new(discordgo.MessageEmbedField)
-// 	fields[4].Name = "Percentage Change 24hr"
-// 	fields[4].Value = fmt.Sprint(coinInfo.PercentChange24h)
-// 	fields[4].Inline = true
-
-// 	fields[5] = new(discordgo.MessageEmbedField)
-// 	fields[5].Name = "Percentage Change 7d"
-// 	fields[5].Value = fmt.Sprint(coinInfo.PercentChange7d)
-// 	fields[5].Inline = true
-
-// 	embed := new(discordgo.MessageEmbed)
-// 	embed.Description = fmt.Sprintf("Grabbing latest data for %s", ticker)
-// 	embed.Fields = fields
-
-// 	_, err = disc.ChannelMessageSendEmbed(channelID, embed)
-// 	if err != nil {
-// 		log.Printf("Error sending message to channel %s: %s", channelID, err)
-// 		return err
-// 	}
-// 	return nil
-// }
+func sendTopTenInfoMessage(disc *discordgo.Session) error {
+	topCoins, err := coinApi.GetAllCoinData(10)
+	if err != nil {
+		_, _ = disc.ChannelMessageSend(channelID, fmt.Sprint("Error getting top coins from coinmarketcap. Please try again later"))
+		return err
+	}
+	_, err = disc.ChannelMessageSend(channelID, generateTopTenMessage(topCoins))
+	if err != nil {
+		log.Printf("Error sending message to channel %s: %s", channelID, err)
+		return err
+	}
+	return nil
+}
