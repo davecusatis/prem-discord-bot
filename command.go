93a9ac73e0df8db98c0f138ad50866dde8aa9c8e
Dave Cusatis <davecusatis1@gmail.com>
2019-10-08 08:11:45 -0700

    fix bot lol

--- command.go
+++ command.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	coinApi "github.com/miguelmota/go-coinmarketcap"
+	cmc "github.com/miguelmota/go-coinmarketcap/pro/v1"
 )
 
 const (
@@ -17,9 +16,6 @@ const (
 	// PriceCheckStockCMD is the command to check coinmarketcap price
 	PriceCheckStockCMD = ".stock"
 
-	// TopTenCMD is the command to check the top 10 on cmc
-	TopTenCMD = ".top"
-
 	// SpaceDelimiter is the delimiter to split commands on
 	SpaceDelimiter = " "
 )
@@ -28,7 +24,7 @@ func messageHandler(disc *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == disc.State.User.ID {
 		return
 	}
-	if !strings.Contains(m.Content, PriceCheckCMD) && !strings.Contains(m.Content, TopTenCMD) && !strings.Contains(m.Content, PriceCheckStockCMD) {
+	if !strings.Contains(m.Content, PriceCheckCMD) && !strings.Contains(m.Content, PriceCheckStockCMD) {
 		return
 	}
 
@@ -56,21 +52,18 @@ func messageHandler(disc *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		return
 	}
-
-	if cmdArray[0] == TopTenCMD {
-		err := sendTopTenInfoMessage(disc, m.ChannelID)
-		if err != nil {
-			log.Printf("Error checking top ten %s", err)
-		}
-		return
-	}
 }
 
-func checkPrice(ticker string) (coinApi.Coin, error) {
-	if val, ok := tickerMap[ticker]; ok {
-		return coinApi.GetCoinData(val)
-	}
-	return coinApi.GetCoinData(ticker)
+func checkPrice(ticker string) ([]*cmc.QuoteLatest, error) {
+	return coinMarketCapClient.Cryptocurrency.LatestQuotes(&cmc.QuoteOptions{
+		Symbol: ticker,
+	})
+	// if val, ok := tickerMap[ticker]; ok {
+	// 	return coinMarketCapClient.Cryptocurrency.LatestQuotes(&cmc.QuoteOptions{
+	// 		Symbol: ticker,
+	// 	})
+	// }
+	// return nil, fmt.Errorf("unknown id")
 }
 
 func sendStockCheckMessage(disc *discordgo.Session, channelID, ticker string) error {
@@ -132,78 +125,46 @@ func sendStockCheckMessage(disc *discordgo.Session, channelID, ticker string) er
 }
 
 func sendPriceCheckMessage(disc *discordgo.Session, channelID, ticker string) error {
-	coinInfo, err := checkPrice(ticker)
+	lower := strings.ToUpper(ticker)
+	coinInfo, err := checkPrice(lower)
 	if err != nil {
 		_, _ = disc.ChannelMessageSend(channelID, fmt.Sprintf("Error getting info for %s from coinmarketcap: %s.", ticker, err.Error()))
 		return err
 	}
 
-	fields := make([]*discordgo.MessageEmbedField, 6)
+	quote := coinInfo[0].Quote["USD"]
+	fields := make([]*discordgo.MessageEmbedField, 5)
 
 	fields[0] = new(discordgo.MessageEmbedField)
 	fields[0].Name = "Price USD"
-	fields[0].Value = fmt.Sprintf("%.2f", coinInfo.PriceUsd)
+	fields[0].Value = fmt.Sprintf("%.2f", quote.Price)
 	fields[0].Inline = true
 
 	fields[1] = new(discordgo.MessageEmbedField)
-	fields[1].Name = "Price BTC"
-	fields[1].Value = fmt.Sprintf("%f", coinInfo.PriceBtc)
+	fields[1].Name = "Market Cap"
+	fields[1].Value = fmt.Sprintf("%.2f", quote.MarketCap)
 	fields[1].Inline = true
 
 	fields[2] = new(discordgo.MessageEmbedField)
-	fields[2].Name = "Market Cap"
-	fields[2].Value = fmt.Sprintf("%.2f", coinInfo.MarketCapUsd)
+	fields[2].Name = "Change 1hr"
+	fields[2].Value = fmt.Sprintf("%.2f%%", quote.PercentChange1H)
 	fields[2].Inline = true
 
 	fields[3] = new(discordgo.MessageEmbedField)
-	fields[3].Name = "Change 1hr"
-	fields[3].Value = fmt.Sprintf("%.2f%%", coinInfo.PercentChange1h)
+	fields[3].Name = "Change 24hr"
+	fields[3].Value = fmt.Sprintf("%.2f%%", quote.PercentChange24H)
 	fields[3].Inline = true
 
 	fields[4] = new(discordgo.MessageEmbedField)
-	fields[4].Name = "Change 24hr"
-	fields[4].Value = fmt.Sprintf("%.2f%%", coinInfo.PercentChange24h)
+	fields[4].Name = "Change 7d"
+	fields[4].Value = fmt.Sprintf("%.2f%%", quote.PercentChange7D)
 	fields[4].Inline = true
-
-	fields[5] = new(discordgo.MessageEmbedField)
-	fields[5].Name = "Change 7d"
-	fields[5].Value = fmt.Sprintf("%.2f%%", coinInfo.PercentChange7d)
-	fields[5].Inline = true
 
 	embed := new(discordgo.MessageEmbed)
 	embed.Description = fmt.Sprintf("Grabbing latest data for %s", ticker)
 	embed.Fields = fields
 
 	_, err = disc.ChannelMessageSendEmbed(channelID, embed)
-	if err != nil {
-		log.Printf("Error sending message to channel %s: %s", channelID, err)
-		return err
-	}
-	return nil
-}
-
-func generateTopTenMessage(coins map[string]coinApi.Coin) string {
-	rankMap := make(map[int]string)
-	for _, coin := range coins {
-		rankMap[coin.Rank] = fmt.Sprintf("%d. %s $%.2f\n", coin.Rank, coin.Name, coin.PriceUsd)
-	}
-
-	i := 1
-	var buffer bytes.Buffer
-	for i <= 10 {
-		buffer.WriteString(rankMap[i])
-		i++
-	}
-	return buffer.String()
-}
-
-func sendTopTenInfoMessage(disc *discordgo.Session, channelID string) error {
-	topCoins, err := coinApi.GetAllCoinData(10)
-	if err != nil {
-		_, _ = disc.ChannelMessageSend(channelID, fmt.Sprint("Error getting top coins from coinmarketcap. Please try again later"))
-		return err
-	}
-	_, err = disc.ChannelMessageSend(channelID, generateTopTenMessage(topCoins))
 	if err != nil {
 		log.Printf("Error sending message to channel %s: %s", channelID, err)
 		return err
--- main.go
+++ main.go
@@ -2,41 +2,43 @@ package main
 
 import (
 	"log"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 	av "github.com/fabianbaier/go-alpha-vantage"
-	coinApi "github.com/miguelmota/go-coinmarketcap"
+	cmc "github.com/miguelmota/go-coinmarketcap/pro/v1"
 )
 
 var (
-	tickerMap    map[string]string
-	mutex        *sync.Mutex
-	ac           *av.Client
-	discordToken string
+	tickerMap           map[string]float64
+	mutex               *sync.Mutex
+	ac                  *av.Client
+	discordToken        string
+	coinMarketCapClient *cmc.Client
 )
 
-func updateTickerMap() {
-	mutex.Lock()
-	res, err := coinApi.GetMarketData()
-	if err != nil {
-		log.Print(err)
-	}
-	coins, err := coinApi.GetAllCoinData(res.ActiveCurrencies)
-	for _, coin := range coins {
-		sym := strings.ToLower(coin.Symbol)
-		tickerMap[sym] = coin.ID
-	}
-	mutex.Unlock()
-}
+// func updateTickerMap() {
+// 	mutex.Lock()
+// 	coins, err := coinMarketCapClient.Cryptocurrency.Map(&cmc.MapOptions{
+// 		ListingStatus: "active",
+// 		Start:         1,
+// 	})
+// 	if err != nil {
+// 		log.Printf("error getting cmc map: %s", err)
+// 	}
+// 	for _, coin := range coins {
+// 		sym := strings.ToLower(coin.Symbol)
+// 		tickerMap[sym] = coin.ID
+// 	}
+// 	mutex.Unlock()
+// }
 
 func main() {
 	parseConfig()
 
 	mutex = &sync.Mutex{}
-	tickerMap = make(map[string]string)
+	tickerMap = make(map[string]float64)
 
 	discordToken = mustGetConfigValue("DISCORD_TOKEN")
 	avToken := mustGetConfigValue("ALPHA_VANTAGE_TOKEN")
@@ -54,19 +56,25 @@ func main() {
 	}
 	discSession.AddHandler(messageHandler)
 
-	go updateTickerMap()
+	cmcToken := mustGetConfigValue("CMC_API_KEY")
+	coinMarketCapClient = cmc.NewClient(&cmc.Config{
+		ProAPIKey: cmcToken,
+	})
+
+	// go updateTickerMap()
 	ticker := time.NewTicker(3600 * time.Second)
 	quit := make(chan struct{})
-	go func() {
+	func() {
 		for ; true; <-ticker.C {
 			select {
 			case <-ticker.C:
 				log.Printf("Syncing with coinmarketcap")
-				go updateTickerMap()
+				// go updateTickerMap()
 			case <-quit:
 				ticker.Stop()
 				return
 			}
 		}
 	}()
+	log.Println("GOT HERE")
 }
